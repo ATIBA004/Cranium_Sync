1f8cdce76b6d24b7148d0d60040c10a1de403b3e
Adegboyega Atiba <qmj439@usask.ca>
2026-04-30 17:48:11 -0600

    Inintial data setup with place holers for test and training data set downloads

---
+++ data.py
@@ -0,0 +1,23 @@
+#this script imports data from yahoo finance
+import yfinance as yf
+
+
+
+symbol = ""
+train_data_start_date = ""
+train_data_end_date = ""
+test_data_start_date = ""
+test_data_end_date = ""
+
+#this function downloads the training data from yahoo finance
+def get_training_data(symbol, train_data_start_date, train_data_end_date):
+    data = yf.download(symbol, start=train_data_start_date, end=train_data_end_date)
+    return data
+
+#this function downloads the test data from yahoo finance
+def get_test_data(symbol, test_data_start_date, test_data_end_date):
+    data = yf.download(symbol, start=test_data_start_date, end=test_data_end_date)
+    return data
+
+
+
